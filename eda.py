92ffe33cd92121d6b358bb96563c3a8153a59374
Valentina Rojas Osorio <valenrojas@ug.uchile.cl>
2024-10-08 12:36:48 -0400

    Update eda.py

--- eda.py
+++ eda.py
@@ -14,7 +14,9 @@ db_config = {
     'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_NAME')
+    'database': os.getenv('DB_NAME'),
+    'charset': 'utf8mb4',
+    'collation': 'utf8mb4_unicode_ci'
 }
 
 # Parámetros de churn
